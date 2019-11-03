901c0c6edfc8a2a4440f4f967e23462c5b34eaed
jonasL96 <47625852+jonasL96@users.noreply.github.com>
2019-11-03 09:59:49 +0200

    Code optimization

--- initial.py
+++ initial.py
@@ -25,7 +25,7 @@ while i < 5:
 i = 0
 q = 0
 r = 0
-v = [0.0] * (len(x) * len(w))
+v = [0.0] * 20
 d = [0.0] * 20
 # Desired output
 while i < len(x):
@@ -33,50 +33,58 @@ while i < len(x):
     i = i + 1
 i = 0
 q = 0
-p = len(x) * len(w)
-y = [0.0] * p
-e = [0.0] * p
+e = [0.0] * 20
+actual = [0.0] * 20
+tmp = [0.0] * 20
 # calculating actual output
 while i < 20:
     while q < 5:
         v[q] = x[i] * w[q] + b[q]
-        y[i] = 1 / (1 + math.exp(v[q]))
+        tmp[q] = 1 / (1 + math.exp(v[q]))
+        actual[i] = actual[i] + tmp[q]
         q = q + 1
     # Calculating error
-    e[i] = d[i] - y[i]
+    e[i] = d[i] - actual[i]
     i = i + 1
     if q == 5:
         q = 0
 e_total = sum(e)
 e_total = round(e_total, 0)  # rounded so there's no additional numbers after comma
-print(e_total)
 #Learning algorithm
 iterations = 2000 #times we will execute this
 i = 0
 r = 0
 q = 0
+de = [0.0] * 20
 while i < iterations:
-    while e_total > 0:
-        while r < 20:
+    r = 0
+    q = 0
+    while r < 20:
+        actual[r] = 0
+        while q < 5:
             #Updating weights
-            while q < 5:
-                st = (-1)*(x[r]*w[q]-b[q])
-                de[q] = (1/(1 + math.exp(st)))*(1-(1/(1+math.exp(st)*e[r])))
-                w[q] = w[q]+eta*de[q]*x[r]
-                b[q] = b[q] + eta*de[q]
-               q = q + 1
-            r = r + 1
-            if q == 5:
-                q = 0
-
-        #calculating y and error with updated weights
-        while o < 20: #to be continued
-            #st = 
-            y[o] = 1/(1+math.exp(st))
-            #error
-            e[o] = d[o] - y[o]
-        #total error
-        e_total = sum(e)
-        e_total = round(e_total, 0)
-        print(e_total)
-
+            st = (-1)*(x[r]*w[q]-b[q])
+            #delta parameter
+            de[q] = (1/(1 + math.exp(st)))*(1-(1/(1+math.exp(st))))*e[r]
+            #rest of the weights
+            w[q] = w[q]+eta*de[q]*x[r]
+            b[q] = b[q] + eta*de[q]
+            # calculating y and error with updated weights
+            st = (-1) * x[r] * w[q] - b[q]
+            tmp[q] = 1/(1+math.exp(st))
+            actual[r] = actual[r] + tmp[q]
+            q = q + 1
+        e[r] = d[r] - actual[r]
+        r = r + 1
+        if q == 5:
+            q = 0
+    #total error
+    e_total = sum(e)
+    e_total = round(e_total, 0)
+    #print(e_total) # for debug
+    i = i + 1
+    if(e_total <= 0 and e_total > -0.01):
+        print("Learning algorithm has found the 0 error solution and it took only %d executions" % (i))
+        break
+    if(i == iterations):
+        print("Could not find a solution.")
